ff60ba72d9862c67c35bf57a8ac6134464d978c6
Marie Junior Oumbiat <109878271+MarieJnr@users.noreply.github.com>
2022-11-19 16:54:18 +0100

    Add files via upload

--- testwebscrapping.py
+++ testwebscrapping.py
@@ -4,10 +4,12 @@ Created on Thu Nov 17 20:57:49 2022
 
 @author: 22541
 """
-
+import time 
 import requests
 from bs4 import BeautifulSoup
 
+
+links=[]
 url ='https://fentybeauty.com/' 
 reponse = requests.get(url)
 print (reponse ) #affiche <Response [200]> si tout s'est bien passé
@@ -18,9 +20,24 @@ if reponse.ok:
       titre=soup.find('title')
       lis=soup.findAll('li') #Trouve tte les balises LI de la page
       for li in lis :
-          a=li.find('a')
-          link = a['href'] #Manque un if pour gerer les balises a qui ne contiennent pas de href
-          links=[]
-          links.append('https://fentybeauty.com/'+ link)
-          print (links)
-         
+          try: # Pour gerer les balises a n'ayant pas de href 
+            a=li.find('a')
+            link = a['href'] #Manque un if pour gerer les balises a qui ne contiennent pas de href
+            links.append('https://fentybeauty.com/'+ link)
+            print (links) 
+          except:
+             pass  
+      time.sleep(3) #Enfin, nous devrions inclure cette ligne de code afin de pouvoir suspendre notre code pendant une seconde afin de ne pas envoyer de requêtes au site Web. Cela nous aide à ne pas être signalé comme spam.
+print (len(links))# voir le nombre de href obtenu
+
+with open ('urls.txt','w') as file : #Creer un fichier texte contenant tout les liens.
+   for link in links :
+    file.write( link + '\n')
+    
+with open ('urls.txt','r') as file : #Lire le fichier contenant ttles liens
+   for row in file :
+    print (row)
+    """
+    a terminer https://youtu.be/KdLkwBNtGsY
+    """
+       
